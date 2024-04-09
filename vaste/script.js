4fd0b3256e31c6b9e38ccaedeb29265511d5748e
uuzor <victor.u.ezealor@gmail.com>
2024-04-09 03:07:58 +0200

    feat: added navBar

--- vaste/script.js
+++ vaste/script.js
@@ -1,13 +1,13 @@
 var image_list = document.querySelector('.image-list');
 var slide_buttons = document.querySelectorAll('.slide-button');
-
+var image_item = document.querySelector('.image-item1');
 
 slide_buttons.forEach(button=>{
     button.addEventListener('click', (e)=>{
         console.log(e.target);
         var direction = e.target.id == 'prev-slide' ? -1 :1;
         
-        var moveby = image_list.clientWidth
+        var moveby = image_item.clientWidth
         console.log(moveby.toFixed(2) * direction);
         console.log(moveby * -1);
         console.log(moveby);
@@ -17,3 +17,8 @@ slide_buttons.forEach(button=>{
         
     })
 })
+
+function PushNav() {
+    const navbar = document.getElementById('nav-bar')
+    navbar.classList.contains('-translate-y-full')? navbar.classList.remove("-translate-y-full"):navbar.classList.add("-translate-y-full"); 
+}
